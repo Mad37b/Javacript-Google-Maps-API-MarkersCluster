ccbf2e22d723d5e5c5107e1ff39f9d250d37b846
Mad37b <Miguelkit37@hotmail.com>
2024-07-26 13:24:28 +0200

    Primer commit: dejamos la base subida , da errores

---
+++ script_map.js
@@ -0,0 +1,33 @@
+
+
+// lugares Portugal 
+
+const locations = [
+  // Albufeira
+  { lat: 37.0893, lng: -8.2470 }, // Praia dos Pescadores
+  { lat: 37.0891, lng: -8.2504 }, // Praia da Oura
+  { lat: 37.0894, lng: -8.2520 }, // Marina de Albufeira
+  { lat: 37.0922, lng: -8.2423 }, // Old Town Albufeira
+  { lat: 37.0929, lng: -8.2495 }, // Igreja Matriz de Albufeira
+
+  // Cabo San Vicente
+  { lat: 36.9780, lng: -9.5097 }, // Cabo de São Vicente Lighthouse
+  { lat: 36.9735, lng: -9.5082 }, // Fortaleza de Sagres
+  { lat: 36.9825, lng: -9.5186 }, // Praia do Beliche
+  { lat: 36.9761, lng: -9.5162 }, // Praia da Mareta
+  { lat: 36.9778, lng: -9.5138 }  // Ponta da Atalaia
+];
+
+
+// iniciar mapa 
+
+const map = 
+
+
+
+// inciar marcador 
+
+
+
+
+// iniciar markercluster s
